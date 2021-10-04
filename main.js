3d351349135994e29515909c1e902bc19834a4bf
Teron Potter <teronpotter@icloud.com>
2021-10-04 14:41:28 -0500

    attempt javascript functions

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 //alert("Javascript has been included successfully");
+
+// //const tasks = [{
+//     content: "Take out the trash.",
+//     completed: true
+//   }, {
+//     content: "Brush your teeth.",
+//     completed: false
+//   }, {
+//     content: "Take over the world.",
+//     completed: false
+// }];
+
+function readForm() {
+    var x = document.getElementById("addForm");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ; i++) {
+        text += x.elements[i].value;
+    }
+    console.log(text);
+}
+
+window.onload = readForm();
