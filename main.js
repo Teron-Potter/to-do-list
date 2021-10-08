d7200e5319767d99492c7b20239b4f5fac5b7fd1
Teron Potter <teronpotter@icloud.com>
2021-10-08 18:43:59 -0500

    Restart JS/Rework CSS

--- main.js
+++ main.js
@@ -1,41 +1,75 @@
-//alert("Javascript has been included successfully");
+// //alert("Javascript has been included successfully");
 
-// //const tasks = [{
-//     content: "Take out the trash.",
-//     completed: true
-//   }, {
-//     content: "Brush your teeth.",
-//     completed: false
-//   }, {
-//     content: "Take over the world.",
-//     completed: false
-// }];
+// // //const tasks = [{
+// //     content: "Take out the trash.",
+// //     completed: true
+// //   }, {
+// //     content: "Brush your teeth.",
+// //     completed: false
+// //   }, {
+// //     content: "Take over the world.",
+// //     completed: false
+// // }];
 
-function readForm() {
-  const formElement = document.querySelector("#addForm");
-  console.dir(formElement[0].value);
-}
+// function readForm() {
+//   const formElement = document.querySelector("#addForm");
+//   console.dir(formElement[0].value);
+// }
 
-function addElement(newTitle) {
-  const newElement = document.createElement("li");
-  newElement.innerHTML = `
-    <div class="item itemDefault">
-      ${newTitle}
-      <div class="deleteButton">
-        <img src="./images/trash.svg" alt="Delete Button" />
-      </div>
-    </div>
-  `;
-  const listElement = document.querySelector("#itemList ul");
-  if (listElement) {
-    listElement.appendChild(newElement);
-  }
-}
+// function addElement(newTitle) {
+//   const newElement = document.createElement("li");
+//   newElement.innerHTML = `
+//     <div class="item itemDefault">
+//       ${newTitle}
+//       <div class="deleteButton">
+//         <img src="./images/trash.svg" alt="Delete Button" />
+//       </div>
+//     </div>
+//   `;
+//   const listElement = document.querySelector("#itemList ul");
+//   if (listElement) {
+//     listElement.appendChild(newElement);
+//   }
+// }
 
-addElement("have buttsecks");
-addElement("have buttsecks");
-addElement("have buttsecks");
-addElement("have buttsecks");
-addElement("have buttsecks");
+// addElement("Added by JS");
 
-window.onload = readForm();
+// window.onload = readForm();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
